dbae61c1778ec062b35e6576dcead01e1ffcd30e
ltphen <tatkeulionel@gmail.com>
2024-04-10 14:39:00 +0200

    Added playground data

---
+++ playground/components/list.tsx
@@ -0,0 +1,5 @@
+export const Item = ({children}: {children: any})=>{
+    return (
+        <div className="text-red-700 p-5 bg-red-50">{children}</div>
+    )
+}
---
+++ playground/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />;
+  </ClerkProvider>
+}
